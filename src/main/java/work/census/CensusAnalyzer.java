a0b34f871c88798adf5dfdcbccce00905b23e21a
Atharva Joshi <iamatharvaj@gmail.com>
2021-06-13 22:09:35 +0530

    Added solution for UC-2.1

--- src/main/java/work/census/CensusAnalyzer.java
+++ src/main/java/work/census/CensusAnalyzer.java
@@ -7,6 +7,7 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.Scanner;
 import java.util.stream.StreamSupport;
 
 import com.opencsv.CSVReader;
@@ -16,7 +17,7 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.exceptions.CsvValidationException;
 
 public class CensusAnalyzer {
-	public int loadIndiaCensusData(String csvFilePath) throws IOException, CensusAnalyzerException {
+	public int loadDataFromCSVFile(String csvFilePath) throws IOException, CensusAnalyzerException {
 		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
 			CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
 			csvToBeanBuilder.withType(IndiaCensusCSV.class);
@@ -26,22 +27,22 @@ public class CensusAnalyzer {
 			Iterable<IndiaCensusCSV> csvIterable = () -> censusCSVIterator;
 			int numOfEntries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
 			return numOfEntries;
-
 		} catch (IOException e) {
-			throw new CensusAnalyzerException(e.getMessage(), CensusAnalyzerException.ExceptionType.CSV_FILE_PROBLEM);
+			throw new CensusAnalyzerException(e.getMessage(),
+					CensusAnalyzerException.ExceptionType.CENSUS_FILE_PROBLEM);
 		} catch (IllegalStateException e) {
 			throw new CensusAnalyzerException(e.getMessage(), CensusAnalyzerException.ExceptionType.UNABLE_TO_PARSE);
 		}
 	}
 
 	@SuppressWarnings("deprecation")
-	public boolean loadIndiaCensusDataFile(String csvFilePath)
-			throws FileNotFoundException, IOException, CensusAnalyzerException, CsvValidationException {
+	public boolean loadIndiaCensusDataFile(String csvFilePath) throws FileNotFoundException, IOException, CensusAnalyzerException, CsvValidationException {
 		CSVReader reader = null;
 		try {
 			reader = new CSVReader(new FileReader(csvFilePath));
 			String[] nextLine;
-			while ((nextLine = reader.readNext()) != null) {
+			while ((nextLine = reader.readNext()) != null) // returns a boolean value
+			{
 				return true;
 			}
 			reader.close();
--- src/test/java/work/census/CensusAnalyzerTest.java
+++ src/test/java/work/census/CensusAnalyzerTest.java
@@ -8,51 +8,62 @@ import org.junit.rules.ExpectedException;
 import com.opencsv.exceptions.CsvValidationException;
 
 public class CensusAnalyzerTest {
-	final String INDIA_CENSUS_CSV_FILE_PATH = "./IndiaCensus.csv";
-	private static final String WRONG_CSV_FILE_PATH = "IndiaCensus.csv";
-	private static final String WRONG_FILE_TYPE = "./IndiaCensus.pdf";
+	private final String INDIAN_CENSUS_CSV_FILE_PATH = "./IndiaCensus.csv";
+	private static final String WRONG_CENSUS_CSV_FILE_PATH = "IndiaCensus.csv";
+	private static final String WRONG_CENSUS_FILE_TYPE_PATH = "./IndiaCensus.pdf";
+	private final String INDIAN_STATE_CODE_CSV_FILE_PATH = "./StateCodes.csv";
 
+	// test for census analysis
 	@Test
 	public void givenIndianCensusCSVFileReturnsCorrectNumOfRecords() throws IOException, CensusAnalyzerException {
 		CensusAnalyzer censusAnalyZer = new CensusAnalyzer();
-		int numOfRecords = censusAnalyZer.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+		int numOfRecords = censusAnalyZer.loadDataFromCSVFile(INDIAN_CENSUS_CSV_FILE_PATH);
 		Assert.assertEquals(1, numOfRecords);
 	}
 
 	@Test
-	public void given_IndiaCensusData_With_Wrong_File_Should_Throw_Exception() throws IOException {
+	public void given_IndiaCensusData_WithWrongFile_ShoulThrewException() throws IOException {
 		try {
-			CensusAnalyzer censusAnalyZer = new CensusAnalyzer();
+			CensusAnalyzer censusAnalyzer = new CensusAnalyzer();
 			ExpectedException exceptionRule = ExpectedException.none();
 			exceptionRule.expect(CensusAnalyzerException.class);
-			censusAnalyZer.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+			censusAnalyzer.loadDataFromCSVFile(WRONG_CENSUS_CSV_FILE_PATH);
 		} catch (CensusAnalyzerException e) {
 			Assert.assertEquals(CensusAnalyzerException.ExceptionType.CSV_FILE_PROBLEM, e.type);
 		}
 	}
 
 	@Test
-	public void given_IndiaCensusData_With_Correct_File_ButWrongType_Should_Throw_Exception() throws IOException {
+	public void given_IndiaCensusData_WithCorrectFile_ButWrongType_ShoulThrewException() throws IOException {
 		try {
 			CensusAnalyzer censusAnalyZer = new CensusAnalyzer();
 			ExpectedException exceptionRule = ExpectedException.none();
 			exceptionRule.expect(IOException.class);
-			censusAnalyZer.loadIndiaCensusData(WRONG_FILE_TYPE);
+			censusAnalyZer.loadDataFromCSVFile(WRONG_CENSUS_FILE_TYPE_PATH);
 		} catch (CensusAnalyzerException e) {
 			Assert.assertEquals(CensusAnalyzerException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
 		}
 	}
 
 	@Test
-	public void given_IndiaCensusData_WithCorrect_File_But_Wrong_Delimiter_Should_Throw_Exception() throws IOException, CsvValidationException {
+	public void given_IndiaCensusData_WithCorrectFile_ButWrongDelimiter_ShoulThrewException() throws IOException, CsvValidationException {
 		try {
-			CensusAnalyzer censusAnalyZer = new CensusAnalyzer();
+			CensusAnalyzer censusAnalyzer = new CensusAnalyzer();
 			ExpectedException exceptionRule = ExpectedException.none();
 			exceptionRule.expect(IOException.class);
-			censusAnalyZer.loadIndiaCensusDataFile(INDIA_CENSUS_CSV_FILE_PATH);
+			censusAnalyzer.loadIndiaCensusDataFile(INDIAN_CENSUS_CSV_FILE_PATH);
 		} catch (CensusAnalyzerException e) {
 			Assert.assertEquals(CensusAnalyzerException.ExceptionType.UNABLE_TO_PARSE, e.type);
 		}
 	}
+
+	// test for state code information
+	@Test
+	public void givenStateCodeCSVFileReturnsCorrectNumOfRecords() throws IOException, CensusAnalyzerException {
+		CensusAnalyzer censusAnalyZer = new CensusAnalyzer();
+		int numOfRecords = censusAnalyZer.loadDataFromCSVFile(INDIAN_STATE_CODE_CSV_FILE_PATH);
+		Assert.assertEquals(1, numOfRecords);
+	}
+	
 
 }
